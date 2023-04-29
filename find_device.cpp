4314ebb790962bd07b2fda95ba6770d713691dd1
TRUONG VAN HOT <truongvanhot98@gmail.com>
2023-04-29 15:39:10 +0900

    first-commit

---
+++ find_device.cpp
@@ -0,0 +1,19 @@
+// Compile with: g++ -o find_device find_device.cpp -lpcap
+// Run with    : ./find_device
+// Result      : Network device found: enp2s0
+#include <iostream>
+#include <pcap.h>
+
+int main(int argc, char *argv[]) {
+    char *device;
+    char error_buffer[PCAP_ERRBUF_SIZE];
+
+    device = pcap_lookupdev(error_buffer);
+    if (device == NULL) {
+        std::cout << "Error finding device: " << error_buffer << std::endl;
+        return 1;
+    }
+    std::cout << "Network device found: " << device << std::endl;
+    return 0;
+}
+
